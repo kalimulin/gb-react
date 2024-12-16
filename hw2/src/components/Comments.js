d9b035697757898163fd8e8ee449385ddd29124a
Rustam Kalimulin <r.kalimulin@jetmail.cc>
2024-12-16 10:46:50 +0400

    Homework 2

--- hw2/src/components/Comments.js
+++ hw2/src/components/Comments.js
@@ -1,65 +1,69 @@
-import { useState } from "react"
+import {useState} from "react"
 
-function Comment (comment, deleteComment) {
-  return (
-      <li className="comments__item" key={comment.id}>
-        <p className="comments__content">{comment.text}</p>
-        <button
-            className="comments__button"
-            onClick={() => deleteComment(comment.id)}
-        >
-          Delete comment
-        </button>
-      </li>
-  )
+function Comment(comment, deleteComment) {
+    return (
+        <li className="comments__item" key={comment.id}>
+            <p className="comments__content">{comment.text}</p>
+            <button
+                className="comments__button"
+                onClick={() => deleteComment(comment.id)}
+            >
+                Delete comment
+            </button>
+        </li>
+    )
 }
 
 function Comments() {
-  const [commentsList, setCommentsList] = useState([])
-  const [comment, setComment] = useState("")
-  const [deletedComments, setDeletedComments] = useState([])
-  const [commentId, setCommentId] = useState(1)
+    const [comments, setComments] = useState([
+        {id: 1, text: "Это первый комментарий"},
+        {id: 2, text: "Это второй комментарий"},
+        {id: 3, text: "Это третий комментарий"}
+    ])
+    const [comment, setComment] = useState("")
+    const [deletedComments, setDeletedComments] = useState([])
+    const [commentId, setCommentId] = useState(4)
 
-  const updateId = () => {
-    setCommentId(commentId + 1)
-  }
+    const updateId = () => {
+        setCommentId(commentId + 1)
+    }
 
-  const addComment = (event) => {
-    event.preventDefault()
-    if (!comment.trim()) return
-    setCommentsList([...commentsList, { id: commentId, text: comment }])
-    updateId()
-    setComment("")
-  }
+    const addComment = (event) => {
+        event.preventDefault()
+        if (!comment.trim()) return
+        setComments([...comments, {id: commentId, text: comment}])
+        updateId()
+        setComment("")
+    }
 
-  const deleteComment = (id) => {
-    setDeletedComments((prevDeletedComments) => {
-      return [...prevDeletedComments, id]
-    })
-  }
+    const deleteComment = (id) => {
+        setDeletedComments((prevDeletedComments) => {
+            return [...prevDeletedComments, id]
+        })
+    }
 
-  return (
-    <div className="comments">
-      <div className="comments__add_box">
-        <input
-          className="comments__add_field"
-          type="text"
-          value={comment}
-          onChange={(event) => setComment(event.target.value)}
-        ></input>
-        <button className="btn-add-comment" onClick={addComment}>
-          Add comment
-        </button>
-      </div>
-      <div className="comments__list_box">
-        <ul className="comments__list_items">
-          {commentsList
-            .filter((comment) => !deletedComments.includes(comment.id))
-            .map((comment) => Comment(comment, deleteComment))}
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div className="comments">
+            <div className="comments__add_box">
+                <input
+                    className="comments__add_field"
+                    type="text"
+                    value={comment}
+                    onChange={(event) => setComment(event.target.value)}
+                ></input>
+                <button className="btn-add-comment" onClick={addComment}>
+                    Add comment
+                </button>
+            </div>
+            <div className="comments__list_box">
+                <ul className="comments__list_items">
+                    {comments
+                        .filter((comment) => !deletedComments.includes(comment.id))
+                        .map((comment) => Comment(comment, deleteComment))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Comments
